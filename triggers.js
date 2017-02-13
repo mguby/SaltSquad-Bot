8f0486f111702163387d4e15f1af2fa1d90fa200
Mark Gubatan <mguby@discord-bots-XDDDD.localdomain>
2017-02-13 22:28:23 +0000

    fix trigger

--- triggers.js
+++ triggers.js
@@ -1,5 +1,5 @@
 module.exports = {
   "kms" : ":weary: :gun:",
-  "making fun of" : "I consider you guys some of my closest friends and would really appreciate it if you would stop making fun of me",
+  "stop making fun of" : "I consider you guys some of my closest friends and would really appreciate it if you would stop making fun of me",
   "i won't drink" : "Sit your ass down Hinako!"
 };
